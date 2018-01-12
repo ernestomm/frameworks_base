d94510c6786f43ef4134899211b6c306bed971c5
ezio84 <brabus84@gmail.com>
2018-01-12 10:54:32 +0100

    Screenrecord service improvements and QSTile [1/2]
    single tap to toggle quality:
    - 480*800 resolution and 1.5Mbps
    - default resolution (720p) and 4Mbps
    - default resolution (720p) and 8Mbps
    long press to start recording
    
    Show also a blinking red button at bottom right of the screen
    that can be tapped to stop the recording instead of using
    the notification.
    
    Also improve the screenrecord service and be sure
    to stop it when we are done.
    
    part 2/2 is in framework/av tree, it just sets max recording
    duration to 30 minutes instead of 3
    
    Change-Id: I9a0161e6ddafb721421585c7f16bba4b30319425
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
+import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -119,6 +120,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ExpandedDesktopTile(mHost);
             case "music":
                 return new MusicTile(mHost);
+            case "screenrecord":
+                return new ScreenrecordTile(mHost);
         }
 
         // Intent tiles.
