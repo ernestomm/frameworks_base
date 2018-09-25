2d473a363f0545d822812d2fca8de344a836a821
xyyx <xyyx@mail.ru>
2018-09-25 11:32:25 +0530

    Ability to disable dashboards conditions (1/2)
    Change-Id: Ia96afe08cf1c5d4fb0001d009d59b39cdf6c4b88

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4740,6 +4740,12 @@ public final class Settings {
         public static final String POWERMENU_LOCKSCREEN = "powermenu_lockscreen";
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
