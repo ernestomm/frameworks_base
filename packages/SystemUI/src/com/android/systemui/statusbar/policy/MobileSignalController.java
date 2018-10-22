b57c5b91c9d01d96e5eaab0b5110196254845d30
SagarMakhar <sagarmakhar@gmail.com>
2018-10-22 18:50:29 +0000

    Add VOLTE icon toggle [1/2]
    Change-Id: I28a981517213509c0a40a815c6e40c695fea6c68
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -90,9 +90,9 @@ public class MobileSignalController extends SignalController<
     private boolean mRoamingIconAllowed;
     private boolean mShow4gForLte;
     private boolean mDataDisabledIcon;
+    private boolean mVoLTEicon;
 
     private ImsManager mImsManager;
-
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
@@ -157,6 +157,9 @@ public class MobileSignalController extends SignalController<
             resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.DATA_DISABLED_ICON), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.SHOW_VOLTE_ICON), false,
+                    this, UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -182,6 +185,10 @@ public class MobileSignalController extends SignalController<
 
         mDataDisabledIcon = Settings.System.getIntForUser(resolver,
                 Settings.System.DATA_DISABLED_ICON, 1,
+                UserHandle.USER_CURRENT) == 1;
+
+        mVoLTEicon = Settings.System.getIntForUser(resolver,
+                Settings.System.SHOW_VOLTE_ICON, 1,
                 UserHandle.USER_CURRENT) == 1;
 
         mapIconSets();
@@ -374,7 +381,7 @@ public class MobileSignalController extends SignalController<
     private int getVolteResId() {
         int resId = 0;
 
-        if ( mCurrentState.imsResitered ) {
+        if ( mCurrentState.imsResitered && mVoLTEicon ) {
             resId = R.drawable.ic_volte;
         }
         return resId;
