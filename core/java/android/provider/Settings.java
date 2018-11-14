7c2df130376b8036ebb01b75446e58241dc81e6c
SuperDroidBond <superdroidbond@yahoo.com>
2018-11-14 12:23:30 +0100

    [SQUASHED] Carrier label and Status Bar clock Font and Color cutoms.. [1/2] Carrier Label font styles [1/2]
    Statusbar Clock Font styles [1/2]
    
    Add Clock size & font style.
    
    Port to Pie - SuperDroidBond
    Clock fonts and size - @temasek
    Port to Oreo by @varund7726
    
    Add Statusbar Clock and Carrier Label color [1/2]
    
    Change-Id: I2db71bbad90d35ba54046882f53917f16bfbebcd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5113,6 +5113,23 @@ public final class Settings {
                 new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
 
         /**
+         * Clock font color
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_COLOR = "status_bar_clock_color";
+
+        /**
+         * Clock font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SIZE = "status_bar_clock_size";
+         /**
+         * Clock font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_FONT_STYLE = "status_bar_clock_font_style";
+
+        /**
          * Sets the date string style
          * 0 - Regular style
          * 1 - Lowercase
@@ -5356,6 +5373,24 @@ public final class Settings {
          * @hide
          */
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+        /**
+         * Carrier Label Custom Color
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
+
+        /**
+         * Settings for carrier label font size
+         * @hide
+         */
+
+        public static final String STATUS_BAR_CARRIER_FONT_SIZE = "status_bar_carrier_font_size";
+         /**
+         * Custom carrier label font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_FONT_STYLE = "status_bar_carrier_font_style";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
