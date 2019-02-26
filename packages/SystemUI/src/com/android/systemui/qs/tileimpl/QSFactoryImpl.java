1f6d8b070766735de5db5147bfa58ba83af3b046
DennySPB <dennyspb@gmail.com>
2019-02-26 09:17:43 +0300

    SystemUI: OneHandMode QS tile
    Change-Id: I89a7d762a22df65fe41b415075e8f59d4265c23d
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
+import com.android.systemui.qs.tiles.OneHandTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
@@ -131,6 +132,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new RebootTile(mHost);
             case "volume_panel":
                 return new VolumeTile(mHost);
+            case "onehand":
+                return new OneHandTile(mHost);
         }
 
         // Intent tiles.
